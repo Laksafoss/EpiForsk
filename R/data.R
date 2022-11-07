b093411f598ce6b144a7f553c36c293619f8f4b1
Anna Damkjær Laksafoss <ADLS@dksund.dk>
2022-11-07 15:44:05 +0100

    andh_forst_plot

---
+++ R/data.R
@@ -0,0 +1,40 @@
+#' Example Data for Husby's Forest Plot Vignette
+#'
+#' A data example for the construction of a multi faceted forest plot.
+#'
+#' @format ## `andh_forest_data`
+#' A data frame with 18 rows and 12 columns:
+#' \describe{
+#'   \item{type}{todo}
+#'   \item{indent}{todo}
+#'   \item{text}{todo}
+#'   \item{A_est}{todo}
+#'   \item{A_l}{todo}
+#'   \item{A_u}{todo}
+#'   \item{B_est}{todo}
+#'   \item{B_l}{todo}
+#'   \item{B_u}{todo}
+#'   \item{C_est}{todo}
+#'   \item{C_l}{todo}
+#'   \item{C_u}{todo}
+#' }
+"andh_forest_data"
+
+
+
+#' Simulated Time-Varying Residence Data
+#'
+#' A dataset of simulated time-varying recidence and gender data.
+#'
+#' @format ## `andh_forest_data`
+#' A data frame with 546 rows and 7 columns describing 100 people:
+#' \describe{
+#'   \item{id}{todo}
+#'   \item{dob}{todo}
+#'   \item{region}{todo}
+#'   \item{move_in}{todo}
+#'   \item{move_out}{todo}
+#'   \item{gender}{todo}
+#'   \item{claim}{todo}
+#' }
+"adls_timevarying_region_data"
