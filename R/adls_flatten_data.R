422f66c34df76bd1f0b18dfe5dd928e056b069b9
Anna Damkjær Laksafoss <ADLS@dksund.dk>
2022-11-11 13:36:00 +0100

    data for adls_flatten and andh_forest

--- R/adls_flatten_data.R
+++ R/adls_flatten_data.R
@@ -12,6 +12,7 @@
 #' expression naming the end date variable in data.
 #' @param lag A numeric, giving the number of days allowed between time
 #' intervals that should be collapsed into one.
+#' @param status 123
 #'
 #' @returns
 #' A data frame with the 'id' and simplified 'in_date' and 'out_date'.
@@ -27,43 +28,52 @@
 #'
 #' @examples
 #'
+#' 1+1
 #' # The flatten function works with both dates and numeric
-#' dat <- data.frame(
-#'    ID    = c(1, 1, 1, 2, 2, 3, 3, 4),
-#'    START = c(1, 2, 5, 3, 6, 2, 3, 6),
-#'    END   = c(3, 3, 7, 4, 9, 3, 5, 8))
-#' dat %>% flatten_date_intervals(ID, START, END)
+#' #dat <- data.frame(
+#'#    ID    = c(1, 1, 1, 2, 2, 3, 3, 4),
+#' #   START = c(1, 2, 5, 3, 6, 2, 3, 6),
+#'  #  END   = c(3, 3, 7, 4, 9, 3, 5, 8))
+#' #dat %>% flatten_date_intervals(ID, START, END)
 #'
-#' dat <- data.frame(
-#'    ID    = c(1, 1, 1, 2, 2, 3, 3),
-#'    START = as.Date(c("2012-02-15", "2005-12-13", "2006-01-24",
-#'                      "2002-03-14", "1997-02-27",
-#'                      2008-08-13", "1998-09-23")),
-#'    END   = as.Date(c("2012-06-03", "2007-02-05", "2006-08-22",
-#'                      "2005-02-26", "1999-04-16",
-#'                      "2008-08-22", "2015-01-29")))
-#' dat %>% flatten_date_intervals(ID, START, END)
+#' #dat <- data.frame(
+#'  #  ID    = c(1, 1, 1, 2, 2, 3, 3),
+#'   # START = as.Date(c("2012-02-15", "2005-12-13", "2006-01-24",
+#'  #                    "2002-03-14", "1997-02-27",
+#'   #                   2008-08-13", "1998-09-23")),
+#'  #  END   = as.Date(c("2012-06-03", "2007-02-05", "2006-08-22",
+#'   #                   "2005-02-26", "1999-04-16",
+#'  #                    "2008-08-22", "2015-01-29")))
+#' #dat %>% flatten_date_intervals(ID, START, END)
 #'
-#' @importFrom rlang .data
-#' @importFrom rlang :=
 #' @export
-flatten_date_intervals <- function(data, id, in_date, out_date, lag = 0) {
-  data %>%
+flatten_date_intervals <- function(data, id, in_date, out_date, status = NULL, lag = 0) {
+  flat <- data %>%
     dplyr::mutate(.numeric_in_date = as.numeric({{ in_date }}),
                   .numeric_out_date = as.numeric({{ out_date }})) %>%
     dplyr::arrange(.data$.numeric_in_date, .data$.numeric_out_date) %>%
-    dplyr::group_by({{ id }}) %>%
+    dplyr::group_by({{ id }}, {{ status }}) %>%
     dplyr::mutate(.internal_running_index = c(
       0,
       cumsum(dplyr::lead(.data$.numeric_in_date) >
                cummax(.data$.numeric_out_date + lag))[-dplyr::n()])) %>%
     dplyr::ungroup() %>%
-    dplyr::group_by({{ id }}, .data$.internal_running_index) %>%
+    dplyr::group_by({{ id }}, {{ status }}, .data$.internal_running_index) %>%
     dplyr::summarize(.interval_IN_DATE = min({{ in_date }}),
                      .interval_OUT_DATE = max({{ out_date }}),
                      .groups = "drop") %>%
     dplyr::select(-.data$.internal_running_index) %>%
     dplyr::rename({{ in_date }} := .data$.interval_IN_DATE,
                   {{ out_date }} := .data$.interval_OUT_DATE)
+  if (lag == 0 | data %>% dplyr::select({{ status }}) %>% ncol == 0) {
+    return(flat)
+  }
+  flat %>%
+    dplyr::mutate(
+      .short = ({{ out_date }} - {{ in_date }} < lag),
+      .first = {{ in_date }} == stats::ave({{ in_date }}, {{ id }}, FUN = min),
+      .last = {{ out_date }} == stats::ave({{ out_date }}, {{ id }}, FUN = max)) %>%
+    dplyr::filter(!.data$.short | .data$.first | .data$.last) %>%
+    dplyr::select(-.data$.short, -.data$.first, -.data$.last)
 }
 
