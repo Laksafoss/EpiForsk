ff8cb394e3055b03c01e5f9e04ebacc651bc5288
Kim Daniel Jakobsen <KIJA@dksund.dk>
2022-11-25 15:32:10 +0100

    Add vignette on writing package functions.

--- R/kija_lms.R
+++ R/kija_lms.R
@@ -186,3 +186,24 @@ lms <- function (formula, data, grp_id, obs_id = NULL, ...)
   class(out) <- c("lms", "lm")
   return(out)
 }
+
+#' @rdname lms
+#' @param x An S3 object with class lms.
+#' @param digits The number of significant digits to be passed to
+#' \link[base]{format}(\link[stats]{coef}(x), .) when \link[base]{print}()ing.
+#' @export
+
+print.lms <- function (x, digits = max(3L, getOption("digits") - 3L), ...)
+{
+  cat("\nNumber of observations: ", nrow(x$data),
+      "\nNumber of groups: ", nrow(x$grp_id %>% dplyr::distinct()),
+      "\n\n", sep = "")
+  if (length(coef(x))) {
+    cat("Coefficients:\n")
+    print.default(format(coef(x), digits = digits), print.gap = 2L,
+                  quote = FALSE)
+  }
+  else cat("No coefficients\n")
+  cat("\n")
+  invisible(x)
+}
