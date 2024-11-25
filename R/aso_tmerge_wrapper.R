f635d15c93bc4016dbef344a595dc8cd68385c3d
kjakobse <kija@ssi.dk>
2024-11-25 14:33:07 +0100

    Initialize tmerge wrapper

---
+++ R/aso_tmerge_wrapper.R
@@ -0,0 +1,35 @@
+#' Create data for Cox-regression
+#'
+#' @param base_data
+#' @param id
+#' @param entry_time
+#' @param exit_time
+#' @param outcome
+#' @param origin
+#' @param scale
+#' @param exposure_data
+#' @param exp_time_var
+#' @param exp_condition_var
+#' @param exp_value_var
+#'
+#' @return
+#'
+#' @author
+#' @examples
+#'
+#' @export
+
+# placeholder name
+tmerge_wrapper <- function(base_data,
+                           id,
+                           entry_time,
+                           exit_time,
+                           outcome,
+                           origin,
+                           scale,
+                           exposure_data,
+                           exp_time_var,
+                           exp_condition_var,
+                           exp_value_var) {
+
+}
